016719e5ecd18f026fccdb8f5dc9d92753896dca
msh <mshdevv@gmail.com>
2024-08-03 19:05:50 -0600

    Update chat

--- main.js
+++ main.js
@@ -1,40 +1,48 @@
 const readline = require("node:readline");
 const OpenAI = require("openai");
+const colors = require("colors");
+
 require("dotenv").config();
 
 const openai = new OpenAI();
 
-//Codigos de color
-
-const blue = "\x1b[1;34m";
-const green = "\x1b[1;32m";
-const clear = "\x1b[0m";
-
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: "User: ",
+  prompt: colors.blue("User: "),
 });
 
-console.log("tty-chatGPT - v 1.0");
+// const history = [];
+
+console.log("tty-chatGPT - v 1.0\n");
 
 async function main() {
   try {
-    rl.prompt()
+    rl.prompt();
     rl.on("line", async (input) => {
+      if (input.trim().toLowerCase() == "exit") {
+        rl.output.write(colors.yellow("Hasta luego.\n"));
+        rl.close();
+        return;
+      }
       const completion = await openai.chat.completions.create({
-        model: "gpt-4",
+        model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: input }],
         stream: true,
       });
 
-      rl.output.write("Chat: ");
+      rl.output.write(colors.green("Chat: "));
+
       for await (const word of completion) {
         rl.output.write(word.choices[0]?.delta?.content || "");
       }
+
+      console.log();
+      rl.prompt();
     });
   } catch (error) {
-    console.error(error);
+    console.error("Error con la respuesta de OpenAI: " + error);
+    rl.close();
   }
 }
 
